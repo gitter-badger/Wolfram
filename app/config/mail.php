9799c39640d3d4e0ccb9a5442d1f2adf692e5ccf
Florian Wartner <info@florianwartner.me>
2015-01-13 14:57:21 +0100

    TODO: Get this information from database.

--- app/config/mail.php
+++ app/config/mail.php
@@ -15,7 +15,7 @@ return array(
 	|
 	*/
 
-	'driver' => 'smtp',
+	'driver' => 'sendmail',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'status@site.com', 'name' => 'Wolfram'),
 
 	/*
 	|--------------------------------------------------------------------------
