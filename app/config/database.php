f9d220f7fe148ba6aa7283462fdd0f8a970442b5
Florian Wartner <info@florianwartner.me>
2015-01-13 14:38:44 +0100

    Edited connection data

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'wolfram',
+			'username'  => 'root',
+			'password'  => 'Hinterlader88!',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
